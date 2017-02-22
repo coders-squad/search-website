eec179a1c93f3bf977a9bede68d9108891b2c630
akram <akram.badah@gmail.com>
2017-02-22 23:46:11 +0200

    frontend editing and removing unneeded consoles

--- server.js
+++ server.js
@@ -5,14 +5,11 @@ var utils = require('./utils.js');
 http.createServer(function(req,res){
 
   var path = req.method +' '+ req.url;
-  console.log(path);
 
   try{
     router[path](req,res);
-        console.log('PATH'+path);
   }catch(err){
     console.log('Error'+err);
-    console.log('PATH'+path);
     res.end('Error Occured');
   }
 }).listen(process.env.PORT || 8079, function(){
